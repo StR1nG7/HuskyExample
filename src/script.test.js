cab40469a701755a36222c972cf1731bf56a6612
ewmb3gt <Vitalii.Stroinov@McKesson.com>
2023-01-10 04:36:00 +0200

    final

--- src/script.test.js
+++ src/script.test.js
@@ -4,7 +4,6 @@ describe('test getSum function', () => {
     it('should return 10', () => {
         const result = getSum(3, 7);
 
-        // встановлюємо невірну суму, щоб тест не пройшов
-        expect(result).toBe(13);
+        expect(result).toBe(10);
     });
 });
